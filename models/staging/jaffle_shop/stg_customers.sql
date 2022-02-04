3c6229106bd95c11a880dd78d5e1fc80cfa2c79b
Leon Shpaner <lshpaner@sandiego.edu>
2022-02-04 17:20:22 +0000

    configured sources, refactored staging models, and checked for source freshness

--- models/staging/jaffle_shop/stg_customers.sql
+++ models/staging/jaffle_shop/stg_customers.sql
@@ -1,12 +1,6 @@
-with customers as (
-
-    select
-        id as customer_id,
-        first_name,
-        last_name
-
-    from `dbt-tutorial`.jaffle_shop.customers
-
-)
-
-select * from customers
+select
+    id as customer_id,
+    first_name,
+    last_name
+    
+from {{ source('jaffle_shop', 'customers')}}
--- models/staging/jaffle_shop/stg_orders.sql
+++ models/staging/jaffle_shop/stg_orders.sql
@@ -1,15 +1,8 @@
-with orders as (
+select
+    id as order_id,
+    user_id as customer_id,
+    order_date,
+    status
 
-    select
-        id as order_id,
-        user_id as customer_id,
-        order_date,
-        status
-
-    from `dbt-tutorial`.jaffle_shop.orders
-
-)
-
-select * from orders
-
+from {{ source('jaffle_shop', 'orders')}}
 
